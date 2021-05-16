f895689b9e136c9f144a8af67662cfd47a169e6f
Fernando Marques <fernandomarques1505@gmail.com>
2021-05-16 13:26:34 -0300

    deleted:    lol/card.sh

--- lol/card.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-xdotool type "fernando" &&
-    xdotool key 23 &&
-    xdotool type "marques" &&
-    xdotool key 23 &&
-    xdotool type "42633412874" &&
-    xdotool key 23 && xdotool type "15052003"
