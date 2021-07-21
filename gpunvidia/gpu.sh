e3d6366a092d728ef047c7dbd543d6c4211f3360
Fernando Marques <fernandomarques1505@gmail.com>
2021-07-21 15:36:01 -0300

    modified:   gpunvidia/gpu.sh

--- gpunvidia/gpu.sh
+++ gpunvidia/gpu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print " ",""$1"""%"}'
+nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print "龍 ",""$1"""%"}'
 
