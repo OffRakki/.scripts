c37b7bdc67c3a88b58020938b1a72b2c161499f5
Fernando Marques <fernandomarques1505@gmail.com>
2021-07-21 18:15:39 -0300

    modified:   eth/eth.sh

--- eth/eth.sh
+++ eth/eth.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+wallet=0x37Cba5798f68684907CB917AFB9dcEbE73187137
+
 eth_in_usd=$(https "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD" | jq '.USD')
 eth_in_brl=$(https "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BRL" | jq '.BRL')
 
-workers=$(https "https://api.flexpool.io/v2/miner/workers?coin=eth&address=0x37Cba5798f68684907CB917AFB9dcEbE73187137" | jq '{ name: .result[].name, online: .result[].isOnline, average_effective_hashrate: .result[].averageEffectiveHashrate, reported_hashrate: .result[].reportedHashrate, valid_hashrates: .result[].validShares }')
+workers=$(https "https://api.flexpool.io/v2/miner/workers?coin=eth&address=$wallet" | jq '{ name: .result[].name, online: .result[].isOnline, average_effective_hashrate: .result[].averageEffectiveHashrate, reported_hashrate: .result[].reportedHashrate, valid_hashrates: .result[].validShares }')
 
-balance_eth=$(bc <<< "scale=6; $(https 'https://api.flexpool.io/v2/miner/balance?coin=eth&address=0x37Cba5798f68684907CB917AFB9dcEbE73187137&countervalue=BRL' | jq '.result.balance') / 1000000000000000000" | awk '{printf "%f", $0}')
-#daily_eth=$(bc <<< "scale=6; $(https GET https://flexpool.io/api/v1/miner/0xB9c434215c09E8c4D6b621E6488D55E1d20CE446/estimatedDailyRevenue/ | jq '.result') / 1000000000000000000" | awk '{printf "%f", $0}')
+balance_eth=$(bc <<< "scale=6; $(https "https://api.flexpool.io/v2/miner/balance?coin=eth&address=$wallet&countervalue=BRL" | jq '.result.balance') / 1000000000000000000" | awk '{printf "%f", $0}')
+#daily_eth=$(bc <<< "scale=6; $(https GET https://flexpool.io/api/v1/miner/$wallet/estimatedDailyRevenue/ | jq '.result') / 1000000000000000000" | awk '{printf "%f", $0}')
 #monthly_eth=$(bc <<< "scale=6; $daily_eth * 30" | awk '{printf "%f", $0}')
 
 balance_usd=$(bc <<< "scale=2; $balance_eth * $eth_in_usd" | awk '{printf "%.2f", $0}')
@@ -31,7 +33,7 @@ if [ "$yesnt" = "y" ]; then
     echo ""
     echo "Workers:"
     echo "$workers"
-    exec https GET "https://api.flexpool.io/v2/miner/payments?coin=eth&address=0x37Cba5798f68684907CB917AFB9dcEbE73187137&countervalue=USD&page=0" | jq '.result'
+    exec https GET "https://api.flexpool.io/v2/miner/payments?coin=eth&address=$wallet&countervalue=USD&page=0" | jq '.result'
 else
     echo ""
     echo "1 ETH = $eth_in_usd USD / $eth_in_brl BRL"
