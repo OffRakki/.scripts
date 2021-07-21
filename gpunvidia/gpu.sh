3c0ae017cf6bc6ac03aca64e0ea1f1fb328098ca
Fernando Marques <fernandomarques1505@gmail.com>
2021-07-21 15:35:19 -0300

    modified:   bkp-pacman-aur/AUR.txt 	modified:   bkp-pacman-aur/pacman.txt 	modified:   gpunvidia/gpu.sh

--- gpunvidia/gpu.sh
+++ gpunvidia/gpu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print "GPU",""$1"""%"}'
+nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print " ",""$1"""%"}'
 
